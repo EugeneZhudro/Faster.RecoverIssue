65636facfb4d67a403347cea2a77ade5e969244a
Eugene Zhudro <eaz@omegasoftware.eu>
2021-12-14 10:13:14 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using FASTER.core;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace RecoverIssue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                var grantsLogPath = Path.Combine("Storage", "hlog.log");
+                var grantsObjLogPath = Path.Combine("Storage", "hlog.obj.log");
+
+                var log = FASTER.core.Devices.CreateLogDevice(grantsLogPath);
+                var objLog = FASTER.core.Devices.CreateLogDevice(grantsObjLogPath);
+
+                var store = new FasterKV<string, List<string>>(
+                        1L << 20,
+                        new LogSettings { LogDevice = log, ObjectLogDevice = objLog },
+                        new CheckpointSettings { CheckpointDir = "Storage", RemoveOutdated = true, CheckPointType = CheckpointType.FoldOver });
+
+               store.Recover();
+                
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
